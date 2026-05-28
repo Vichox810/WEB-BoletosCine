aff3813a648ff600213ad66d919cd9841faf0aac
Vichoto <vice.moralesv@gmail.com>
2026-05-28 13:48:34 -0400

    Agrego modelo Pelicula con migracion

---
+++ Boletos_cine/back/migrations/20260528174427-create-pelicula.js
@@ -0,0 +1,40 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Peliculas', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      titulo: {
+        type: Sequelize.STRING
+      },
+      genero: {
+        type: Sequelize.STRING
+      },
+      duracion: {
+        type: Sequelize.INTEGER
+      },
+      sinopsis: {
+        type: Sequelize.STRING
+      },
+      imagen: {
+        type: Sequelize.STRING
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Peliculas');
+  }
+};
