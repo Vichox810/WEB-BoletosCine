9a6f9e2f0e2117f4d7624835dfea7afc06835da8
Vichoto <vice.moralesv@gmail.com>
2026-06-01 14:33:17 -0400

    Arreglo

--- Boletos_cine/back/migrations/20260528175941-create-funcion.js
+++ Boletos_cine/back/migrations/20260528175941-create-funcion.js
@@ -2,7 +2,7 @@
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface, Sequelize) {
-    await queryInterface.createTable('Funcions', {
+    await queryInterface.createTable('Funcion', {
       id: {
         allowNull: false,
         autoIncrement: true,
