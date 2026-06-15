87039dfec4e66bd4f17d41d1b4b06274b227991a
Vichoto <vice.moralesv@gmail.com>
2026-06-15 14:59:37 -0400

    fix: interceptor 401 para mostrar mensaje amigable

--- Boletos_cine/front/src/api.js
+++ Boletos_cine/front/src/api.js
@@ -5,4 +5,17 @@ const api = axios.create({
   baseURL: API_URL
 })
 
+api.interceptors.response.use(
+  res => res,
+  err => {
+    if (err.response?.status === 401) {
+      const originalMsg = err.response?.data?.error || ''
+      if (originalMsg === 'Token requerido' || originalMsg === 'Token inválido o expirado') {
+        err.response.data.error = 'Debes iniciar sesión para realizar esta acción'
+      }
+    }
+    return Promise.reject(err)
+  }
+)
+
 export default api
